4a4cd80edf47d10e831f6fe4e7c0111ee75ca70d
aliciasavelly <alicia.savelly@gmail.com>
2017-02-10 15:44:56 -0800

    add attribute_values method and continue work on insert method

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -3,9 +3,17 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    DBConnection.execute2( "select * from #{self.table_name}" )[0].map do |el|
-      el.to_sym
-    end
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+      LIMIT
+        0
+    SQL
+
+    @columns = cols.map { |el| el.to_sym }
   end
 
   def self.finalize!
@@ -79,11 +87,28 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    attribute_vals = []
+
+    self.class.columns.each do |column|
+      attribute_vals << send(column)
+    end
+
+    attribute_vals
   end
 
   def insert
-    # ...
+    num_vals = attribute_values.length - 1
+    columns = self.class.columns.drop(1)
+    col_names = columns.map { |el| el.to_s }.join(", ")
+
+    DBConnection.instance.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        "#{self.class.table_name} (#{col_names})"
+      VALUES
+        (["?"] * num_vals)
+    SQL
+
+    DBConnection.last_insert_row_id
   end
 
   def update
