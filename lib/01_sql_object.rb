9ebd60983a201f7a4756917d495b81171280fdf5
aliciasavelly <alicia.savelly@gmail.com>
2017-02-10 12:03:40 -0800

    add self.columns

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,7 +5,9 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    
+    DBConnection.execute2( "select * from #{self.table_name}" )[0].map do |el|
+      el.to_sym
+    end
   end
 
   def self.finalize!
@@ -36,7 +38,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    
   end
 
   def attribute_values
